572793c98c56f644bb25b5ea8c9acddb6ec2a9f7
nathancroce <nathan.croce@mymail.champlain.edu>
2024-09-05 16:08:42 -0400

    week2

--- week2/ProcessManagement.ps1
+++ week2/ProcessManagement.ps1
@@ -1,8 +1,25 @@
 ﻿#9/5/24
 
-# List every process for which ProcessName starts with 'C'
+# 1. List every process for which ProcessName starts with 'C'
 Get-Process | Where-Object { $_.Name -ilike "C*"}
 
-#list every process for which the path does not include the string "system32"
+# 2. List every process for which the path does not include the string "system32"
 Get-Process | Where-Object { $_.Name -inotlike "*system32*"}
+
+# 3. List every stopped service, order alphabetically, and export to csv
+Get-Service | Where-Object { $_.Status -eq "Stopped" } | `
+Sort-Object | Export-Csv -Path StoppedServices.csv
+
+# 4. If google chrome browser is not running, start it and direct to champlain.edu
+#    If it is already running, stop it
+if (Get-Process -Name chrome -ErrorAction SilentlyContinue){
+    Write-Host "Chrome Running. Stopping now"
+    Get-Process -name chrome | Stop-Process
+}
+else{
+    Write-Host "Chrome not running. Starting now"
+    Start-Process 'C:\Program Files\Google\Chrome\Application\chrome.exe' `
+    '--new-window https://champlain.edu'
+}
+
 

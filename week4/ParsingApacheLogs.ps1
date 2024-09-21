9191b7bc9698f8ac73e5370fde1f20d700ea05a9
nathancroce <nathan.croce@mymail.champlain.edu>
2024-09-21 13:46:37 -0400

    week 4

---
+++ week4/ParsingApacheLogs.ps1
@@ -0,0 +1,24 @@
+﻿
+function ApacheLogs1(){
+$logsnotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tablerecords = @()
+
+$logsnotformatted.Length
+
+for($i=0, $i -lt $logsnotformatted.Count, $i++){
+
+$word = $logsnotformatted[$i].Split(" ")
+
+$tablerecords += [pscustomobject]@{"IP"="$word[0]"; `
+                                   "Time"="Time"; `
+                                   "Method"="method"; `
+                                   "Page"="page"; `
+                                   "Protocol"="prot"; `
+                                   "Response"="resp"; `
+                                   "Referrer"="ref"; `
+                                   "Client"="client"; }
+        }# end of for loop
+return $tablerecords | Where-Object { $_.IP -ilike "10.*"}
+}
+$tablerecords = ApacheLogs1
+$tablerecords
---
+++ week4/WindowsApacheLogs.ps1
@@ -0,0 +1,24 @@
+﻿# list all the apache logs of XAMPP
+#Get-Content C:\xampp\apache\logs\access.log
+
+# list last 5 lines of the apache logs of XAMPP
+#Get-Content C:\xampp\apache\logs\access.log -Tail 5
+
+# display only 404 and 400 errors
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ', ' 400 '
+
+# display all logs that are NOT code 200
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 '
+
+# from every file with .log extension, get those that contain the word '-error'
+#$A = Get-ChildItem C:\xampp\apache\logs\*.log | Select-String 'error'
+# select last 5 from array
+#$A[-5..-1]
+
+# display ip addresses for 404 errors
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+$notfounds
+$regex = [regex] "\d{1-3}.\d{1-3}.\d{1-3}.\d{1-3}"
+$ipsunorganized = [regex]::Matches($notfounds, $regex)
+$ipsunorganized
+

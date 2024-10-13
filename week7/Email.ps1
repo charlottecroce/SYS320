2a39cde6cba8cf89a13018201ce592d875e1409e
Charlotte Croce <90940521+charlottecroce@users.noreply.github.com>
2024-10-13 14:27:04 -0400

    Update Email.ps1

--- week7/Email.ps1
+++ week7/Email.ps1
@@ -5,7 +5,7 @@ $To = "charlotte.croce@mymail.champlain.edu"
 $Subject = "Suspicious Activity"
 
 # remove password before publishing to GitHub!
-$Password = "fcdh onxc nwzf pday" | ConvertTo-SecureString -AsPlainText -Force
+$Password = "xxxx" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" `
